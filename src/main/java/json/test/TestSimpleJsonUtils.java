bdd4bc0eb5cd672e3e4f7669b06ca469af9b4226
13967778930 <1216774549@qq.com>
2024-03-24 18:53:14 +0800

    添加手写json工具类

---
+++ src/main/java/json/test/TestSimpleJsonUtils.java
@@ -0,0 +1,19 @@
+package json.test;
+
+import json.WendyJsonUtils;
+
+public class TestSimpleJsonUtils {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John Doe");
+        person.setAge(30);
+
+        // 序列化
+        String json = WendyJsonUtils.serialize(person);
+        System.out.println(json);
+
+        // 反序列化
+        Person personDeserialized = WendyJsonUtils.deserialize(json, Person.class);
+        System.out.println(personDeserialized.getName() + ", " + personDeserialized.getAge());
+    }
+}
